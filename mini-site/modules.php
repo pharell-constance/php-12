b3151fe94f45ec511dc454d9dba1e046d4b604bc
æth3rboy <pharellc.dev@gmail.com>
2025-07-24 09:47:00 +0200

    new commit

--- mini-site/modules.php
+++ mini-site/modules.php
@@ -11,7 +11,23 @@ require_once 'includes/header.php';
 
     </li>
     <h1>Module2</h1>
+    <li>
+        <ul>Qu’est ce-qu’une variable ?</ul>
+        <ul>Qu’est ce qu’une constante ?</ul>
+        <ul>Les types primitifs</ul>
+        <ul>Afficher du texte</ul>
+        <ul>Concaténer</ul>
+
+    </li>
     <h1>Module3</h1>
+    <ul>les différents opérateurs</ul>
+    <ul>Les opérateurs arithmétiques</ul>
+    <ul>Les opérateurs d’affectation</ul>
+    <ul>Les opérateurs de comparaison</ul>
+    <ul>Les opérateurs d’incrémentation et de décrémentation</ul>
+    <ul>Les opérateurs logiques</ul>
+    <ul>Les opérateurs de chaînes de caractères</ul>
+
     <h1>Module4</h1>
     <h1>Module5</h1>
     <h1>Module6</h1>
